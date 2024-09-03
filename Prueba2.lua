0ef5f34c2f9aedb58ccc8bb148a261674f25c65b
Fernanflop091o <162623537+Fernanflop091o@users.noreply.github.com>
2024-09-02 22:01:54 -0500

    Update Prueba2.lua

--- Prueba2.lua
+++ Prueba2.lua
@@ -1,4 +1,4 @@
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Fernanflop091o/PARA_AMIGOS-ASER-SCRITP/main/Prueba.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Fernanflop091o/PARA_AMIGOS-ASER-SCRITP/main/Prueba3.lua"))()
 
 local player = game.Players.LocalPlayer
 	local data = game.ReplicatedStorage.Datas[player.UserId]
