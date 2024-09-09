0afe40e0567b9ac79e764b8983a5e8d69a027f64
Fernanflop091o <162623537+Fernanflop091o@users.noreply.github.com>
2024-09-08 22:38:37 -0500

    Create Fer2.lua

---
+++ Fer2.lua
@@ -0,0 +1,20 @@
+
+local player = game.Players.LocalPlayer
+
+-- Verifica si el jugador está en el lugar con el ID especificado
+if game.PlaceId == 5151400895 then
+    local data = game.ReplicatedStorage.Datas[player.UserId]
+    if data.Strength.Value >= 8000000 then
+        wait(4)
+        game:GetService("ReplicatedStorage").Package.Events.equipskill:InvokeServer("Godly SSJ2")
+        wait(1)
+        game:GetService("ReplicatedStorage").Package.Events.ta:InvokeServer()
+    end
+    wait(.01)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Fernanflop091o/PARA_AMIGOS-ASER-SCRITP/main/Fern.lua"))()                                    
+else
+    -- Si no está en el lugar especificado, ejecuta directamente el loadstring
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Fernanflop091o/PARA_AMIGOS-ASER-SCRITP/main/Fern.lua"))()
+end
+
+
